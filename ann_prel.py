f2235710c472854676cddec158df3bb6d4b8d5fe
anuj-badhwar <anuj.badhwar@outlook.com>
2017-06-13 17:01:21 +0530

    Added files

---
+++ ann_prel.py
@@ -0,0 +1,47 @@
+#Artificial Neural Network
+
+#Install Theano -- Numerical Computation - Can also run on GPU
+#Install Tensorflow -- Numerical Computation - By Google can also run on GPU
+#Install Keras -- Build Deep Learning Models in fewer lines of code -Based on Theano and Tensorflow
+
+#Todo : Models. Dummy variables set karne hai.
+
+#Part1 - Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1 ].values
+
+#Encoding Categorical Data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X1 = LabelEncoder()
+X[:, 1] = labelencoder_X1.fit_transform(X[:, 1])
+labelencoder_X2 = LabelEncoder()
+X[:, 2] = labelencoder_X2.fit_transform(X[:, 2])
+
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
